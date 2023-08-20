d6c86cde3c6d539942eb324499ae0f7219faebb5
Inella12138 <141432262+Inella12138@users.noreply.github.com>
2023-08-20 11:50:58 +0800

    1

---
+++ Polymorphism/Account.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_PartII
+{
+    public class Account
+    {
+        private string acctNumber;
+        private string acctHolderId;
+        private double balance;
+
+        //Constructor
+        public Account(string acctNumber, string acctHolderId, double balance)
+        {
+            this.acctNumber = acctNumber;
+            this.acctHolderId = acctHolderId;
+            this.balance = balance;
+        }
+
+        //Properties
+        public string AccountNumber
+        {
+            get { return acctNumber; }
+        }
+
+        public string AccountHolderId
+        {
+            get { return acctHolderId; }
+            set { acctHolderId = value; }
+        }
+
+        public double Balance
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+
+        //Methods
+
+        //overdraft account可以无条件取款，其他账户需要balance > 0
+        //定义一个virtual的withdraw()方法可以让程序共用一个方法名
+        public virtual bool Withdraw(double amt)
+        {
+            balance -= amt;
+            return true;
+        }
+
+        public void Deposit(double amt)
+        {
+            balance += amt;
+        }
+
+        public bool TransferTo(double amt,Account another)
+        {
+            bool enoughMoney;
+            enoughMoney = Withdraw(amt);
+            if (enoughMoney)
+            {
+                another.balance += amt;
+            }
+            return enoughMoney;
+        }
+
+        public override string ToString()
+        {
+            string t;
+            t = "Account:\nAccount Number = "+AccountNumber+" Account Holder ID = "+
+                AccountHolderId+" Balance = "+ Balance+"\n";
+            return t;
+        }
+
+        public virtual string NotShowId()
+        {
+            string t;
+            t = "Account: Account Number = " + AccountNumber + " Balance = " + Balance + "\n";
+            return t;
+        }
+
+        //在母类中定义一个CalculateInterest()方法，此处并无作用，只是为了让子类能够override
+        //若不在此定义一个virtual方法，每个子类(saving,current，overdraft)
+        //中的CalculateInterest()方法重名，可能引起潜在问题
+        public virtual double CalculateInterest()
+        {
+            return 0;
+        }
+
+        //加利息函数，各子类中都相同，直接定义在母类中
+        public void CreditInterest()
+        {
+            Deposit(CalculateInterest());
+        }
+    }
+}
---
+++ Polymorphism/Test.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Inheritance_PartII;
+
+namespace OOPCS_Workshop_Polymorphism
+{
+   public class Test
+   {
+        public static void Main()
+        {
+            /**
+             * TODO: create a branch, add accounts and  
+             * call methods to test.
+             * 
+             * You many uncomment each part of the following 
+             * code to use for your testing. You're encouraged
+             * to add your own test cases.
+             */
+
+            Account savingAccount = new SavingAccount("0000111", "S1111111A", 2000);
+            Console.WriteLine(savingAccount.ToString());
+            Account currentAccount = new CurrentAccount("0000222", "S2222222B", 2000);
+            Console.WriteLine(currentAccount.ToString());
+            Account overdraftAccount1 = new OverdraftAccount("0000333", "S3333333C", 2000);
+            Console.WriteLine(overdraftAccount1.ToString());
+            Account overdraftAccount2 = new OverdraftAccount("0000444", "S1111111A", -2000);
+            Console.WriteLine(overdraftAccount2.ToString());
+            Account overdraftAccount3 = new OverdraftAccount("0000555", "S3333333C", 2000);
+            Console.WriteLine(overdraftAccount3.ToString());
+
+            BankBranch branch = new BankBranch("KOKO Bank Branch");
+            branch.AddAccount(savingAccount);
+            branch.AddAccount(currentAccount);
+            branch.AddAccount(overdraftAccount1);
+            branch.AddAccount(overdraftAccount2);
+            branch.AddAccount(overdraftAccount3);
+
+            Console.WriteLine();
+            Console.WriteLine("Print all accounts");
+            branch.PrintAccounts();
+
+            Console.WriteLine();
+            Console.WriteLine("Total deposits: {0}", branch.TotalDeposits());
+            Console.WriteLine("Total interest paid: {0}", branch.TotalInterestPaid());
+            Console.WriteLine("Total interest earned: {0}", branch.TotalInterestEarned());
+
+            Console.WriteLine();
+            Console.WriteLine("Print all customers");
+            branch.PrintCustomers();
+
+            Console.ReadLine();
+
+        }
+   }
+}
