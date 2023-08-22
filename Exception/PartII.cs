331cd399957b37f53e0847d9176925b453ac8f37
Inella12138 <141432262+Inella12138@users.noreply.github.com>
2023-08-22 13:24:32 +0800

    part2

---
+++ Exception/PartII.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_PartII
+{
+    public class ExcPropagation
+    {
+        public void M1()
+        {
+            try
+            {
+                Console.WriteLine("Enter try block of M1.");
+                M2();
+                Console.WriteLine("Exit try block of M1.");
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine("Enter catch block of M1.");
+                Console.WriteLine("Exception from: {0}", e.TargetSite);
+                Console.WriteLine("Exit catch block of M1.");
+            }
+            Console.WriteLine("Exit M1.");
+        }
+        public void M2()
+        {
+            Console.WriteLine("Enter M2.");
+            int y = 0;
+            int x = 10 / y;
+            Console.WriteLine("Exit M2.");
+        }
+    }
+
+}
---
+++ Exception/PartII_Test.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_PartII
+{
+    class TestPropagation
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter Main.");
+            ExcPropagation mc = new ExcPropagation();
+            try
+            {
+                mc.M1();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Enter catch block of Main.");
+                Console.WriteLine("Exception from: {0}",
+                           e.TargetSite);
+                Console.WriteLine("Exit catch block of Main.");
+            }
+            Console.WriteLine("Exit Main.");
+        }
+    }
+
+}
