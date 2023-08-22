1bc0edca4838417bf18dc7575c3df8b196dfec52
Inella12138 <141432262+Inella12138@users.noreply.github.com>
2023-08-22 13:05:52 +0800

    exception

---
+++ Exception/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_PartI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter three sides of a triangle to calculate its perimeter and area.");
+                Console.Write("Please enter the length of the first side: ");
+                double side1 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Please enter the length of the second side: ");
+                double side2 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Please enter the length of the third side: ");
+                double side3 = Convert.ToDouble(Console.ReadLine());
+
+                Triangle tri = new Triangle(side1, side2, side3);
+                Console.WriteLine("The perimeter of the triangle is: {0}.\nThe area of the triangle is: {1}"
+                    ,tri.Perimeter(),tri.Area());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Sorry, your input is not in the correct format!");
+            }
+            catch (BadTriangleException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine("End of program.");
+        }
+    }
+}
---
+++ Exception/Triangle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_PartI
+{
+    class Triangle
+    {
+        private double side1, side2, side3;
+
+        public Triangle(double side1, double side2, double side3) 
+        {
+            if (IsTriangle(side1, side2, side3))
+            {
+                this.side1 = side1;
+                this.side2 = side2;
+                this.side3 = side3;
+            }
+            else
+            {
+                throw new BadTriangleException("Invalid sides!");
+            }
+        }
+
+        static bool IsTriangle(double side1, double side2, double side3)
+        {
+            bool flag = false;
+            if (side1 > 0 && side2 > 0 && side3 > 0)
+            {
+                if (side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1)
+                {
+                    flag = true;
+                }
+            }
+            return flag;
+        }
+
+        public double Perimeter()
+        {
+            return side1 + side2 + side3;
+        }
+
+        public double Area()
+        {
+            double s = (side1 + side2 + side3) / 2;
+            double area = Math.Sqrt(s * (s - side1) * (s - side2) * (s - side3));
+            return area;
+        }
+    }
+}
